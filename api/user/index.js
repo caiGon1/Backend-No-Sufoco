55f19443de2f1be34db247c21038976433f7c554
Caio Silva <caio.goncalvesananias@gmail.com>
2026-05-15 11:53:14 -0300

    Add user registration endpoint and restructure user API

--- api/user/index.js
+++ api/user/index.js
@@ -4,16 +4,11 @@ export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("NoSufocoDB"); //inicializa o banco de dados NoSufocoDB
 
-  if (req.method === 'POST') {
-    const { nome, email, senha, banco} = req.body; // Recebendo os dados do corpo da requisição
-    
-    // Inserindo o usuário
-    const resultado = await db.collection("users").insertOne({ nome, email, senha, banco }); // Inserindo o usuário na coleção "users"
-    
-    // Retornamos o ID gerado
-    return res.status(201).json({ 
-      mensagem: "Usuário criado!", 
-      idCriado: resultado.insertedId 
+  if (req.method === 'GET') {
+    return res.status(200).json({ 
+        status: "Online",
+        sector: "User API",
+        message: "Backend Serverless e MongoDB integrados com sucesso!" 
     });
   }
 }
