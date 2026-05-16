45a41c89004794a4712a6434664e482f5a0c5e89
Caio Silva <caio.goncalvesananias@gmail.com>
2026-05-16 10:48:34 -0300

    Refactor MongoDB connection and integrate Google GenAI service: remove unused code, update environment variable usage, and create new API handler

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import clientPromise from '../lib/mongodb.js'; // Importa a conexão otimizada
-
+import { main }  from '../src/service/index.js'; // Importa a função principal do serviço de IA
 export default async function handler(req, res) {
   try {
     // Em vez de 'new MongoClient', usamos o client que vem da lib
@@ -8,9 +8,12 @@ export default async function handler(req, res) {
     // Faz o ping para garantir que a ponte está de pé
     await client.db("admin").command({ ping: 1 });
 
+    const resposta = await main(); // Chama a função principal do serviço de IA
+
     return res.status(200).json({ 
       status: "Online", 
       message: "Conexão via LIB funcionando com sucesso!",
+      resposta: resposta
 
     });
   } catch (e) {
--- src/service/index.js
+++ src/service/index.js
@@ -1,37 +1,14 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"; // Verifique se o import está assim
+import { GoogleGenAI } from "@google/genai";
 
-export default async function gerarRespostaIA() {
-  const key = process.env.ai;
+const key = process.env.GOOGLE_API_KEY;
 
-  // O nome da classe no pacote oficial é GoogleGenerativeAI
-  const genAI = new GoogleGenerativeAI(key);
+const ai = new GoogleGenAI({ apiKey: key });
 
-  const textoDoExtrato = `
-  02/05/2026   COMPRA CARTAO - Supermercado BH   -R$ 150,20
-  05/05/2026   PIX RECEBIDO - Joao Silva         +R$ 500,00
-  08/05/2026   DOC ELETRONICO - Netflix          -R$ 55,90
-  `;
+export async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents:"Write 'If you can read this, it works!'",
+  });
 
-  try {
-    // 1. Mudamos para o modelo estável: gemini-1.5-flash
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-    const prompt = `
-      Organize o seguinte extrato bruto em uma tabela de texto limpa (.txt). 
-      Use o formato: DATA | DESCRIÇÃO | VALOR
-      Não adicione introduções nem explicações.
-      Extrato:
-      ${textoDoExtrato}
-    `;
-
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    
-    // 2. O texto é uma função: .text()
-    return response.text();
-
-  } catch (erro) {
-    console.error("Erro na IA:", erro);
-    throw new Error("Erro ao processar IA: " + erro.message);
-  }
+  return response.text;
 }
