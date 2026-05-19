aacf0e9f35f2efb768f744d25978b238c6186035
Caio Silva <caio.goncalvesananias@gmail.com>
2026-05-19 13:08:59 -0300

    feat: expand CORS configuration to include additional origins for production and development

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,12 @@
 export default function cors(req, res) {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://no-sufoco.vercel.app"
+    "https://no-sufoco.vercel.app",
+    "https://no-sufoco.vercel.app/cadastro",
+    "https://no-sufoco.vercel.app/dashboard",
+    "http://localhost:5173",
+    "http://localhost:5173/cadastro",
+    "http://localhost:5173/dashboard"
   );
 
   res.setHeader(
